fb3f2f1cff9ba51bca142149844f348c5c1fe450
Cristian Baldi <baddy227@gmail.com>
2026-01-10 00:02:47 +0100

    mod build config

--- src/controls/Potentiometer.cpp
+++ src/controls/Potentiometer.cpp
@@ -2,8 +2,6 @@
 
 #include <math.h>
 
-#define M_E 2.71828182845904523536 /* e */
-
 extern const float POT_DEADZONE;
 
 extern ILI9341_t3 tft;
--- src/modules/LFO.cpp
+++ src/modules/LFO.cpp
@@ -1,4 +1,5 @@
 #include "LFO.h"
+#include <string>
 
 
 namespace {
