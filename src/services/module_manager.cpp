b353e56e8e1383e3044a1620395eb3d87c01d077
Cristian Baldi <baddy227@gmail.com>
2026-01-15 22:13:13 +0100

    refactor module names

--- src/services/module_manager.cpp
+++ src/services/module_manager.cpp
@@ -11,6 +11,8 @@ void ModuleManager::factory()
 
 		uint_fast8_t moduleType = ModuleTypeIdMux(slotAddress).getModuleId();
 
+		Module* module = nullptr;
+
 		switch (moduleType)
 		{
 
@@ -18,106 +20,94 @@ void ModuleManager::factory()
 
 		case ModuleTypeIds::AUDIO_OUT:
 		{
-			logModuleDetected(moduleType, "AUDIO_OUT", slotAddress);
-			activeModules.push_back(new AudioOut(slotAddress));
+			module = new AudioOut(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::AUDIO_IN:
 		{
-			logModuleDetected(moduleType, "AUDIO_IN", slotAddress);
-			activeModules.push_back(new AudioIn(slotAddress));
+			module = new AudioIn(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::FILTER:
 		{
-			logModuleDetected(moduleType, "VCF", slotAddress);
-			activeModules.push_back(new VCF(slotAddress));
+			module = new VCF(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::VCO_ID:
 		{
-			logModuleDetected(moduleType, "VCO_det", slotAddress);
-			activeModules.push_back(new VCO_det(slotAddress));
+			module = new VCO_det(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::MIDI_MODULE:
 		{
-			logModuleDetected(moduleType, "MIDI_MONO", slotAddress);
-			activeModules.push_back(new MidiMono(slotAddress));
+			module = new MidiMono(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::ADSR:
 		{
-			logModuleDetected(moduleType, "ADSR", slotAddress);
-			activeModules.push_back(new ADSR(slotAddress));
+			module = new ADSR(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::MIXER8:
 		{
-			logModuleDetected(moduleType, "MIXER 8", slotAddress);
-			activeModules.push_back(new Mixer8(slotAddress));
+			module = new Mixer8(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::VCA:
 		{
-			logModuleDetected(moduleType, "VCA", slotAddress);
-			activeModules.push_back(new VCA(slotAddress));
+			module = new VCA(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::DRUMS:
 		{
-			logModuleDetected(moduleType, "DRUMZ", slotAddress);
-			activeModules.push_back(new DrumMachine(slotAddress));
+			module = new DrumMachine(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::DELAY:
 		{
-			logModuleDetected(moduleType, "DELAY MULTI", slotAddress);
-			activeModules.push_back(new Delay_multi(slotAddress));
+			module = new Delay_multi(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::DISTORTION:
 		{
-			logModuleDetected(moduleType, "DISTORTION_bc", slotAddress);
-			activeModules.push_back(new Distortion_bc(slotAddress));
+			module = new Distortion_bc(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::REVERB:
 		{
-			logModuleDetected(moduleType, "REVERB", slotAddress);
-			activeModules.push_back(new Reverb(slotAddress));
+			module = new Reverb(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::DISTORTION_AMP:
 		{
-			logModuleDetected(moduleType, "DISTORTION_amp", slotAddress);
-			activeModules.push_back(new Distortion_amp(slotAddress));
+			module = new Distortion_amp(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::DELAY_SINGLE:
 		{
-			logModuleDetected(moduleType, "DELAY_SINGLE", slotAddress);
-			activeModules.push_back(new Delay_single(slotAddress));
+			module = new Delay_single(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::LOOPER:
 		{
-			logModuleDetected(moduleType, "LOOPER", slotAddress);
-			activeModules.push_back(new Looper(slotAddress));
+			module = new Looper(slotAddress);
 			break;
 		}
 		case ModuleTypeIds::LFO:
 		{
-			logModuleDetected(moduleType, "LFO", slotAddress);
-			activeModules.push_back(new LFO(slotAddress));
+			module = new LFO(slotAddress);
 			break;
 		}
 		default:
 		{
-			logModuleDetected(moduleType, "UNKNOWN", slotAddress);
 			break;
 		}
 		};
+
+		if (module != nullptr) {
+			logModuleDetected(moduleType, module->getModuleName(), slotAddress);
+			activeModules.push_back(module);
+		}
 
 		#if CONFIGURATION__LOGGER__MODULE_FACTORY
 		if (MAX_MODULES > 16 && slotAddress.toInt() % 16 == 15)
